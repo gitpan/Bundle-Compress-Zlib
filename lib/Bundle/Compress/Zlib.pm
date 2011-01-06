c109525bade8389b3eacda428509772ed2b2a062
Paul Marquess <pmqs@cpan.org>
2011-01-06 11:21:54 +0000

    Import of PMQS/Bundle-Compress-Zlib-2.032 from CPAN.
    gitpan-cpan-distribution: Bundle-Compress-Zlib
    gitpan-cpan-version:      2.032
    gitpan-cpan-path:         PMQS/Bundle-Compress-Zlib-2.032.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/Bundle/Compress/Zlib.pm
+++ lib/Bundle/Compress/Zlib.pm
@@ -2,7 +2,7 @@ package Bundle::Compress::Zlib;
 
 use strict;
 
-$Bundle::Compress::Zlib::VERSION = '2.030';
+$Bundle::Compress::Zlib::VERSION = '2.032';
 
 1;
 __END__
@@ -19,11 +19,11 @@ Bundle::Compress::Zlib - Install Compress::Zlib and dependencies
 
 Scalar::Util
 
-Compress::Raw::Zlib 2.030
+Compress::Raw::Zlib 2.032
 
-Compress::Raw::Bzip2 2.030
+Compress::Raw::Bzip2 2.032
 
-Compress::Zlib 2.030
+Compress::Zlib 2.032
 
 =head1 AUTHOR
 
@@ -33,7 +33,7 @@ Version > 0.3  Paul Marquess <pmqs@cpan.org>
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2007 by Kevin W. Henwood
-Copyright (C) 2008-2010 by Paul Marquess
+Copyright (C) 2008-2011 by Paul Marquess
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
