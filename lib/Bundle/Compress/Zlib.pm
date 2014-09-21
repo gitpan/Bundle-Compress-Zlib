76169f8f323dd802e4bd478f5a7e5a332e57d621
Paul Marquess <pmqs@cpan.org>
2014-09-21 12:40:35 +0000

    Import of PMQS/Bundle-Compress-Zlib-2.066 from CPAN.
    gitpan-cpan-distribution: Bundle-Compress-Zlib
    gitpan-cpan-version:      2.066
    gitpan-cpan-path:         PMQS/Bundle-Compress-Zlib-2.066.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/Bundle/Compress/Zlib.pm
+++ lib/Bundle/Compress/Zlib.pm
@@ -2,7 +2,7 @@ package Bundle::Compress::Zlib;
 
 use strict;
 
-$Bundle::Compress::Zlib::VERSION = '2.064';
+$Bundle::Compress::Zlib::VERSION = '2.066';
 
 1;
 __END__
@@ -19,11 +19,11 @@ Bundle::Compress::Zlib - Install Compress::Zlib and dependencies
 
 Scalar::Util
 
-Compress::Raw::Zlib 2.064
+Compress::Raw::Zlib 2.066
 
-Compress::Raw::Bzip2 2.064
+Compress::Raw::Bzip2 2.066
 
-Compress::Zlib 2.064
+Compress::Zlib 2.066
 
 =head1 AUTHOR
 
